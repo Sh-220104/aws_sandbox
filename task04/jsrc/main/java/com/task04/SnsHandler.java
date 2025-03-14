3cfd38f1b76e10e2bf869212b3f592a93040971d
Vuppala Shreya <vuppala_shreya@epam.com>
2025-03-14 11:38:52 +0530

    updated

--- task04/jsrc/main/java/com/task04/SnsHandler.java
+++ task04/jsrc/main/java/com/task04/SnsHandler.java
@@ -10,25 +10,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 @LambdaHandler(
-		lambdaName = "sns_handler",
-		roleName = "sns_handler-role",
-		isPublishVersion = true,
-		aliasName = "learn",
-		logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+        lambdaName = "sns_handler",
+        roleName = "sns_handler-role",
+        isPublishVersion = true,
+        aliasName = "learn",
+        logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
 @SnsEventSource(
-		targetTopic = "lambda_topic"
+        targetTopic = "lambda_topic"
 )
 public class SnsHandler implements RequestHandler<Object, Map<String, Object>> {
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
-		context.getLogger().log("Received event: " + request.toString() + "\n");
+    public Map<String, Object> handleRequest(Object request, Context context) {
+        context.getLogger().log("Received event: " + request.toString() + "\n");
 
-		Map<String, Object> resultMap = new HashMap<>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("body", "Processed SNS Messages Successfully");
+        Map<String, Object> resultMap = new HashMap<>();
+        resultMap.put("statusCode", 200);
+        resultMap.put("body", "Processed SNS Messages Successfully");
 
-
-		return resultMap;
-	}
+        return resultMap;
+    }
 }
